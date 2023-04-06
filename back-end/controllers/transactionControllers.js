72584617ac7b75603edb81daa83214ebb40e9ccd
Muhamad Danang Priambodo <68808411+mcdanang@users.noreply.github.com>
2023-04-06 21:19:22 +0700

    Update remaining_ticket logic

--- back-end/controllers/transactionControllers.js
+++ back-end/controllers/transactionControllers.js
@@ -4,7 +4,7 @@ const { Op, Sequelize } = require('sequelize');
 const { validationResult } = require("express-validator");
 const transaction = db.Transaction;
 const event = db.Event;
-const user = db.User;
+// const user = db.User;
 
 module.exports = {
   createTransaction: async (req, res) => {
@@ -21,22 +21,33 @@ module.exports = {
       
       if (!event_id || !ticket_qty) throw "Please complete your form"
 
-      const ticketSum = await transaction.sum('ticket_qty', { where: { user_id, event_id } });
-      console.log(ticketSum);
-      if (ticketSum >= 3) throw 'You have reach limit 3 tickets'
-      if (ticketSum + ticket_qty > 3) throw  `You can only buy ${3 - ticketSum} ticket(s)`
+      // const ticketSum = await transaction.sum('ticket_qty', { where: { user_id, event_id } });
+      // console.log(ticketSum);
+      // if (ticketSum >= 3) throw 'You have reach limit 3 tickets'
+      // if (ticketSum + ticket_qty > 3) throw  `You can only buy ${3 - ticketSum} ticket(s)`
 
       const eventData = await event.findOne({
         where: {
           id: event_id
         }
       })
+
+      if (eventData.remaining_ticket == 0) throw `Ticket sold out`
+      if (eventData.remaining_ticket < ticket_qty) throw `Ticket almost sold out. You can only buy ${eventData.remaining_ticket} tickets left`
+
       const total_price = eventData.price * ticket_qty;
 
       const data = await transaction.create({
         ...req.body,
         user_id,
         total_price
+      });
+
+      await event.decrement("remaining_ticket", {
+        by: ticket_qty,
+        where: {
+          id: event_id
+        }
       });
   
       res.status(200).send({
